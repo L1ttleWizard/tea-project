150384bb68edcb0859ced88745e5dc564974c73d
Andrey <andrwyandreev2005@gmail.com>
2023-02-15 17:48:20 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash'
 import './scss/styles.scss'
 import jQuery from 'jquery'
+import "./jq"
+import "./fontawesome.js"
 
 
 // or, specify which plugins you need:
 import Popover from 'bootstrap'
 import bootstrap from 'bootstrap'
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
